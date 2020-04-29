4d75f3cccfadc00872a40dadf8f5545732ec5f4a
eXhumer <exhumer1@protonmail.com>
2020-04-29 00:44:30 -0600

    Add function to get user client secret file. Modified helper functions to take client_id/client_secret.

--- TinGen/eXDrive.py
+++ TinGen/eXDrive.py
@@ -1,15 +1,18 @@
-from google.auth.transport.requests import AuthorizedSession
-from requests import Session
-from google.auth.transport.requests import Request
+from abc import ABCMeta
+from abc import abstractmethod
 from google.auth.credentials import Credentials
+from google.auth.transport.requests import AuthorizedSession
+from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials as UserCredentials
 from google.oauth2.service_account import Credentials as SACredentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from pathlib import Path
-from typing import Sequence
-from abc import ABCMeta
-from abc import abstractmethod
 from requests import Response
+from requests import Session
+from typing import Sequence
+from json import load as json_deserialize
+from json import dump as json_serialize
+from json import JSONDecodeError
 
 
 class AuthHelpers(object, metaclass=ABCMeta):
@@ -31,19 +34,69 @@ class AuthHelpers(object, metaclass=ABCMeta):
 
     @staticmethod
     def generate_user_credentials_from_client_secrets_file(
-        credentials_path: Path,
+        client_id: str,
+        client_secret: str,
         scopes: Sequence[str],
         **options
     ) -> UserCredentials:
-        if not credentials_path.is_file():
-            raise FileNotFoundError("Unable to create user credentials without client secret file.")
         if scopes is None or len(scopes) == 0:
             raise Exception("No scope specified.")
-        auth_flow = InstalledAppFlow.from_client_secrets_file(str(credentials_path), scopes)
+        auth_flow = InstalledAppFlow.from_client_config({"installed": {"client_id": client_id, "client_secret": client_secret}})
         if "console" in options and options["console"]:
             return auth_flow.run_console(**options)
         else:
             return auth_flow.run_local_server(**options)
+
+    @staticmethod
+    def generate_user_credentials(
+        client_secret_path: Path,
+        scopes_needed: Sequence[str],
+        **options
+    ) -> UserCredentials:
+        if not client_secret_path.is_file():
+            raise FileNotFoundError(f"Client secret file {str(client_secret_path)} doesn't exist. Unable to proceed without valid client secret file.")
+        else:
+            with open(client_secret_path, "r") as client_secret_stream:
+                try:
+                    client_secret = json_deserialize(client_secret_stream)
+                except JSONDecodeError:
+                    raise FileNotFoundError(f"Client secret file {str(client_secret_path)} doesn't contain valid client secret JSON data. Unable to proceed without valid client secret file.")
+
+        credentials = None
+        token_path = options.get("token_path", None)
+
+        if token_path is not None and token_path.is_file():
+            try:
+                with open(token_path, "r") as token_stream:
+                    token = json_deserialize(token_stream)
+                    if "refresh_token" in token.keys():
+                        credentials = UserCredentials(
+                            token.get("access_token", default=None),
+                            refresh_token=token["refresh_token"],
+                            client_id=client_secret["installed"]["client_id"],
+                            client_secret=client_secret["installed"]["client_secret"]
+                        )
+            except JSONDecodeError:
+                pass
+
+        if credentials is None:
+            credentials = AuthHelpers.generate_user_credentials_from_client_secrets_file(client_secret["installed"]["client_id"], client_secret["installed"]["client_secret"], scopes_needed, **options)
+
+        print(f"DEBUG: scopes = {credentials.scopes}")
+        if not credentials.valid:
+            if not credentials.expired:
+                credentials.refresh(Request())
+            else:
+                credentials = AuthHelpers.generate_user_credentials_from_client_secrets_file(client_secret_path, credentials.scopes, **options)
+
+        if not credentials.has_scopes(scopes_needed):
+            credentials = AuthHelpers.generate_user_credentials_from_client_secrets_file(client_secret_path, scopes_needed.extend(credentials.scopes), **options)
+
+        if token_path is not None:
+            with open(token_path, "w") as token_stream:
+                json_serialize({"access_token": credentials.token, "refresh_token": credentials.refresh_token}, token_stream)
+
+        return credentials
 
     @staticmethod
     def get_new_authenticated_session(
@@ -101,10 +154,6 @@ class AuthDriveV3Service(AbstractDriveV3Service):
 
     def delete_file(self, file_id: str) -> Response:
         return self.session.request("DELETE", f"{self.SERVICE_URI}/files/{file_id}", params={"supportsAllDrives": True})
-
-    def download_file(self, file_id: str):
-        # TODO
-        pass
 
 
 class DriveV3Service(AbstractDriveV3Service):
